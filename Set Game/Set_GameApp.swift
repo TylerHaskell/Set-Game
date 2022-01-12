1864a903bf94f3dab7cfb770b36375724d30e830
Tyler Haskell <tyler.haskell@concur.com>
2022-01-12 11:17:45 -0600

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Haskell, Tyler on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
